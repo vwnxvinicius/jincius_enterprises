07de9391988bc704bc4a2141b67bce85e9c55daf
vwnx1 <vwnx123@gmail.com>
2021-02-03 18:59:27 -0300

    Criando o joguinho com ursina

--- okok.py
+++ okok.py
@@ -6,19 +6,19 @@ from ursina import *
 app = Ursina()
 
 
+cube = Entity(model = 'Quad', scale = (1.5,1), position = (0,3),collider = 'box', texture = 'lado_b.png')
 
-cube = Entity(model = 'Quad', scale = (1.5,1), position = (0,3), texture = 'lado_b.png')
-circle = Entity(model = 'Quad', scale = (1.5,1),position = (0,-3), texture = 'lado_c.png')
+bg = Entity(model = 'Quad', texture = 'bg.jpeg', scale = (15,8))
+circle = Entity(model = 'Quad', scale = (1.5,1),position = (0,-3),collider = 'box', texture = 'lado_c.png')
+
+ammo = Entity(model = 'Circle', scale = (0.1,0.1),collider = 'box', color = color.yellow)
 
 def update():
     cube.x -= held_keys['a'] * .1
     cube.x += held_keys['d'] * .1
-    cube.y -= held_keys['s'] * .1
-    cube.y += held_keys['w'] * .1
     circle.x -= held_keys['j'] * .1
     circle.x += held_keys['l'] * .1
-    circle.y -= held_keys['k'] * .1
-    circle.y += held_keys['i'] * .1
+    ammo.y += .1
 
 
 
